c817323880dffa2d985a65c6c40b8cf4a3dbe150
maryg <maryg@LAPTOP-20U3V9TB>
2026-01-15 15:33:58 -0600

    Add project files.

---
+++ activity1/activity1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Dogs {
+private:
+	string name;
+	int age;
+	string breed;
+public:
+	void setName(string dogname) {
+		name = dogname;
+	}
+	void setAge(int dogage) {
+		age = dogage;
+	}
+	void setBreed(string dogbreed)
+	{
+		breed = dogbreed;
+	}
+	string getName()
+	{
+		return name;
+	}
+	int getAge()
+	{
+		return age;
+	}
+	string getBreed()
+	{
+		return breed;
+	}
+	void bark()
+	{
+		cout << "Woof! Woof!" << endl;
+	}
+};
+
+int main()
+{
+	Dogs dog1;
+
+	dog1.setName("Bruno");
+	dog1.setAge(6);
+	dog1.setBreed("Pit bull");
+
+	cout << "Dog Properties" << endl;
+	cout << "Name: " << dog1.getName() << endl;
+	cout << "Age: " << dog1.getAge() << endl;
+	cout << "Breed: " << dog1.getBreed() << endl << endl;
+
+	dog1.bark();
+
+	return 0;
+}
